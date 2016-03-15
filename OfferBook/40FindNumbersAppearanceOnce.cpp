020942f51cbfdd3c453f7c238a6ba0c047259acf
wanglaiqi <1191580068@qq.com>
2016-03-15 20:06:48 +0800

    位运算

--- OfferBook/40FindNumbersAppearanceOnce.cpp
+++ OfferBook/40FindNumbersAppearanceOnce.cpp
@@ -1,91 +1,98 @@
-#include <iostream>
-#include <stdio.h>
+#include<iostream>
+#include<stdio.h>
 using namespace std;
 
-unsigned int FindFirstBitIs1(int num);
-bool IsBit1(int num, unsigned int indexBit);
-
-void FindNumsAppearOnce(int data[], int length, int* num1, int* num2)
+unsigned int findFirstBitIs1(int num);
+bool IsBit(int num,unsigned int indexBit);
+/*找出数组中只出现一次的数字*/
+void findNumberAppearanceOnce(int data[],int length,int *num1,int *num2)
 {
-    if (data == NULL || length < 2)
-        return;
- 
-    int resultExclusiveOR = 0;
-    for (int i = 0; i < length; ++ i)
-        resultExclusiveOR ^= data[i];
- 
-    unsigned int indexOf1 = FindFirstBitIs1(resultExclusiveOR);    
- 
-    *num1 = *num2 = 0;
-    for (int j = 0; j < length; ++ j)
+    if(data==NULL||length<0)
     {
-        if(IsBit1(data[j], indexOf1))
-            *num1 ^= data[j];
+        return;
+    }
+    int resultExecuteOr = 0;
+    for(int i = 0;i<length;i++)
+    {
+        resultExecuteOr^=data[i];
+    }
+    unsigned int indexOf1 = findFirstBitIs1(resultExecuteOr);
+    *num1 = 0;
+    *num2 = 0;
+    for(int j = 0;j<length;j++)
+    {
+        if(IsBit(data[j],indexOf1))
+        {
+            *num1^=data[j];
+        }
         else
-            *num2 ^= data[j];
+        {
+            *num2^=data[j];
+        }
     }
 }
- 
-//find 1 
-unsigned int FindFirstBitIs1(int num)
+
+/*返回第一位是1的下标*/
+unsigned int findFirstBitIs1(int num)
 {
     int indexBit = 0;
-    while (((num & 1) == 0) && (indexBit < 8 * sizeof(int)))
+    while(((num&1) == 0)&&(indexBit < 8*sizeof(int)))
     {
-        num = num >> 1;
-        ++ indexBit;
+        indexBit++;
+        num = num>>1;
     }
- 
     return indexBit;
 }
 
-//is 1
-bool IsBit1(int num, unsigned int indexBit)
+//找到num从右边数第一位是1的位置
+bool IsBit(int num,unsigned int indexBit)
 {
-    num = num >> indexBit;
-    return (num & 1);
+    num = num>>indexBit;
+    return (num&1);
 }
 
-//test
-void Test(char* testName, int data[], int length, int expected1, int expected2)
+/*测试代码*/
+void Test(const char *testName,int data[],int length,int expected1,int expected2)
 {
-    if(testName != NULL)
-        printf("%s begins: ", testName);
+    if(testName!=NULL)
+    {
+        printf("%s begins:",testName);
+    }
+    int result1,result2;
+    findNumberAppearanceOnce(data,length,&result1,&result2);
 
-    int result1, result2;
-    FindNumsAppearOnce(data, length, &result1, &result2);
-
-    if((expected1 == result1 && expected2 == result2) ||
-        (expected2 == result1 && expected1 == result2))
+    if((expected1==result1&&expected2==result2)||(expected2==result1&&expected1==result2))
+    {
         printf("Passed.\n\n");
+    }
     else
+    {
         printf("Failed.\n\n");
+    }
+
 }
 
 void Test1()
 {
-    int data[] = {2, 4, 3, 6, 3, 2, 5, 5};
-    Test("Test1", data, sizeof(data) / sizeof(int), 4, 6);
+    int data[] = {2,4,3,6,3,2,5,5};
+    Test("Test1",data,sizeof(data)/sizeof(int),4,6);
 }
 
 void Test2()
 {
-    int data[] = {4, 6};
-    Test("Test2", data, sizeof(data) / sizeof(int), 4, 6);
+    int data[] = {4,6};
+    Test("Test2",data,sizeof(data)/sizeof(int),4,6);
 }
 
 void Test3()
 {
-    int data[] = {4, 6, 1, 1, 1, 1};
-    Test("Test3", data, sizeof(data) / sizeof(int), 4, 6);
+    int data[] = {4,6,1,1,1,1};
+    Test("Test3",data,sizeof(data)/sizeof(int),4,6);
 }
-
 int main()
 {
     Test1();
     Test2();
     Test3();
-
     return 0;
 }
-
--- OfferBook/47Add.cpp
+++ OfferBook/47Add.cpp
@@ -1,98 +1,41 @@
 #include<iostream>
-#include<stdio.h>
 using namespace std;
 
-unsigned int findFirstBitIs1(int num);
-bool IsBit(int num,unsigned int indexBit);
-/*找出数组中只出现一次的数字*/
-void findNumberAppearanceOnce(int data[],int length,int *num1,int *num2)
+/*不适用加减乘除做加法运算*/
+int add(int num1,int num2)
 {
-    if(data==NULL||length<0)
+    int sum = 0;
+    int carray = 0;
+    do
     {
-        return;
-    }
-    int resultExecuteOr = 0;
-    for(int i = 0;i<length;i++)
-    {
-        resultExecuteOr^=data[i];
-    }
-    unsigned int indexOf1 = findFirstBitIs1(resultExecuteOr);
-    *num1 = 0;
-    *num2 = 0;
-    for(int j = 0;j<length;j++)
-    {
-        if(IsBit(data[j],indexOf1))
-        {
-            *num1^=data[j];
-        }
-        else
-        {
-            *num2^=data[j];
-        }
-    }
-}
+        sum = num1^num2;
+        carray = (num1&num2)<<1;
 
-/*返回第一位是1的下标*/
-unsigned int findFirstBitIs1(int num)
-{
-    int indexBit = 0;
-    while(((num&1) == 0)&&(indexBit < 8*sizeof(int)))
-    {
-        indexBit++;
-        num = num>>1;
+        num1 = sum;
+        num2 = carray;
     }
-    return indexBit;
-}
-
-//找到num从右边数第一位是1的位置
-bool IsBit(int num,unsigned int indexBit)
-{
-    num = num>>indexBit;
-    return (num&1);
+    while(num2!=0);
+    return num1;
 }
 
 /*测试代码*/
-void Test(const char *testName,int data[],int length,int expected1,int expected2)
+void Test(int num1,int num2,int expected)
 {
-    if(testName!=NULL)
+    int result = add(num1,num2);
+    if(result = expected)
     {
-        printf("%s begins:",testName);
-    }
-    int result1,result2;
-    findNumberAppearanceOnce(data,length,&result1,&result2);
-
-    if((expected1==result1&&expected2==result2)||(expected2==result1&&expected1==result2))
-    {
-        printf("Passed.\n\n");
+        cout<<num1<<"+"<<num2<<"="<<result<<"passed"<<endl;
     }
     else
     {
-        printf("Failed.\n\n");
+        cout<<num1<<"+"<<num2<<"="<<result<<"Failed"<<endl;
     }
-
-}
-
-void Test1()
-{
-    int data[] = {2,4,3,6,3,2,5,5};
-    Test("Test1",data,sizeof(data)/sizeof(int),4,6);
-}
-
-void Test2()
-{
-    int data[] = {4,6};
-    Test("Test2",data,sizeof(data)/sizeof(int),4,6);
-}
-
-void Test3()
-{
-    int data[] = {4,6,1,1,1,1};
-    Test("Test3",data,sizeof(data)/sizeof(int),4,6);
 }
 int main()
 {
-    Test1();
-    Test2();
-    Test3();
+    Test(1,2,3);
+    Test(111,899,1010);
+    Test(3,0,3);
+    Test(-2,-8,-10);
     return 0;
 }
